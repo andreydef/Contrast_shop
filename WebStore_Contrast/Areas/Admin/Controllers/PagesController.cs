648a8bcf2f50d66bc60194bf2682ec9cef9a047f
AndriyHalelyuka <43058552+AndriyHalelyuka@users.noreply.github.com>
2020-01-23 19:52:32 +0200

    Realize 2 methods in admin-area (Edit, Details)
    I realize 2 methods in admin-area (Edit, Details) and testing this methods in admin-area

--- WebStore_Contrast/Areas/Admin/Controllers/PagesController.cs
+++ WebStore_Contrast/Areas/Admin/Controllers/PagesController.cs
@@ -93,5 +93,122 @@ namespace WebStore_Contrast.Areas.Admin.Controllers
             // Redirected the user to the method INDEX
             return RedirectToAction("Index");
         }
+
+        // GET: Admin/Pages/EditPage/id
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            // Create models PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                // Get page data
+                PagesDTO dto = db.Pages.Find(id);
+
+                // Checking, if a page is available
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                // Initialize model dates (Ініціалізуємо модель данними)
+                model = new PageVM(dto);
+
+                // Return model
+                return View(model);
+            }
+        }
+
+        // POST: Admin/Pages/EditPage/id
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            // Check model for validity
+            if (!ModelState.IsValid) // if model does not valid
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                // Get ID of the page
+                int id = model.Id;
+
+                // Create variable for short title (temporary variable)
+                string slug = "home";
+
+                // Get page (by id)
+                PagesDTO dto = db.Pages.Find(id);
+
+                // Assign a title for the resulting model to DTO
+                dto.Title = model.Title;
+
+                // Check the short title and assign it, if necessary
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug)) // null or space
+                    {
+                        slug = model.Title;
+                    }
+                    else
+                    {
+                        slug = model.Slug;
+                    }
+                }
+
+                // Check sluh and title in unique
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title))  // Search the database for all ID in addition to the ID that is entered at the moment,
+                                                                                       // and if Title matches the database with what we entered then we add an error
+                {
+                    ModelState.AddModelError("", "That title already exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "That slug already exist.");
+                    return View(model);
+                }
+
+                // Write other values to the class DTO
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                // Save Changes in database
+                db.SaveChanges();
+            }
+
+            // Send notifications through the TempData
+            TempData["SM"] = "You have esited the page.";
+
+            // Redirected the user
+            return RedirectToAction("EditPage");
+        }
+
+        // GET: Admin/Pages/EditPage/id
+        public ActionResult PageDetails(int id)
+        {
+            // Assign model PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                // Get page
+                PagesDTO dto = db.Pages.Find(id);
+
+                // Confirm, that page is available 
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                // Assogn to model information from database
+                model = new PageVM(dto);
+            }
+
+            // Return model in View
+            return View(model);
+        }
     }
 }
