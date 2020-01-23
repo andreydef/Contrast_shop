8f9a2cc11c6fdce724345a28d6e1b63d82f88fe6
AndriyHalelyuka <43058552+AndriyHalelyuka@users.noreply.github.com>
2020-01-23 23:05:13 +0200

    Updating with add a goods into the page
    I updated with add a goods into the page:
    
    1. Implement a database table, classes, controller and view of adding goods;
    2. Realize post-methods to adding a goods;
    3. Realize the method and view of the list of goods, in wich be sorting by categories and page navigation;
    4. Add NuGet package PagesList.Mvc to adding a pages navigation;
    5. Realize method for list of goods;
    6. Add and realize functions of page navigation ;
    7. Add image if don't take image;
    8. Write JavaScript code to work with page navigation;
    9. Test with adding of goods in page, them sortimg and page navigation;

--- WebStore_Contrast/Areas/Admin/Controllers/ShopController.cs
+++ WebStore_Contrast/Areas/Admin/Controllers/ShopController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
+using PagedList;
 using WebStore_Contrast.Models.Data;
 using WebStore_Contrast.Models.ViewModels.Shop;
 
@@ -11,6 +14,7 @@ namespace WebStore_Contrast.Areas.Admin.Controllers
     public class ShopController : Controller
     {
         // GET: Admin/Shop
+        [HttpGet]
         public ActionResult Categories()
         {
             // Declare model with type List
@@ -91,6 +95,7 @@ namespace WebStore_Contrast.Areas.Admin.Controllers
 
         // Add GET method to Delete Category
         // GET: Admin/Shop/DeleteCategory/id
+        [HttpGet]
         public ActionResult DeleteCategory(int id)
         {
             using (Db db = new Db())
@@ -136,6 +141,190 @@ namespace WebStore_Contrast.Areas.Admin.Controllers
 
             // Return string
             return "ok";
+        }
+
+        // Add GET method to Adding goods
+        // GET: Admin/Shop/AddProduct
+        [HttpGet]
+        public ActionResult AddProduct()
+        {
+            // Assign the model of data
+            ProductVM model = new ProductVM();
+
+            // Add the list of categories from database to model
+            using (Db db = new Db())
+            {
+                model.Categories = new SelectList(db.Categories.ToList(), "id", "Name");
+            }
+
+            // Return model in view()
+            return View(model);
+        }
+
+        // Add POST method to Adding goods
+        // POST: Admin/Shop/AddProduct
+        [HttpPost]
+        public ActionResult AddProduct(ProductVM model, HttpPostedFileBase file)
+        {
+            // Check model in validation
+            if (!ModelState.IsValid)
+            {
+                using (Db db = new Db())
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    return View(model);
+                }
+            }
+
+            // Check the product name for unicity
+            using (Db db = new Db())
+            {
+                if (db.Products.Any(x => x.Name == model.Name))
+                {
+                    model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                    ModelState.AddModelError("", "The product name is taken!");
+                    return View(model);
+                }
+            }
+
+            // Assign variable ProductID
+            int id;
+
+            // Initialize and save model on base ProductDTO
+            using (Db db = new Db())
+            {
+                ProductDTO product = new ProductDTO();
+
+                product.Name = model.Name;
+                product.Slug = model.Name;
+                product.Description = model.Description;
+                product.Price = model.Price;
+                product.CategoryId = model.CategoryId;
+
+                CategoryDTO catDTO = db.Categories.FirstOrDefault(x => x.Id == model.CategoryId);
+                product.CategoryName = catDTO.Name;
+
+                db.Products.Add(product);
+                db.SaveChanges();
+
+                id = product.Id;
+            }
+
+            // Add message in TempData
+            TempData["SM"] = "You have added a product!";
+
+            #region Upload Image
+
+            // Create the necessary links of directories
+            var originalDirectory = new DirectoryInfo(string.Format($"{Server.MapPath(@"\")}Images\\Uploads"));
+
+            var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+            var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs");
+            var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+            var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+            // Check availability of directories (if not, create)
+            if (!Directory.Exists(pathString1))
+                Directory.CreateDirectory(pathString1);
+
+            if (!Directory.Exists(pathString2))
+                Directory.CreateDirectory(pathString2);
+
+            if (!Directory.Exists(pathString3))
+                Directory.CreateDirectory(pathString3);
+
+            if (!Directory.Exists(pathString4))
+                Directory.CreateDirectory(pathString4);
+
+            if (!Directory.Exists(pathString5))
+                Directory.CreateDirectory(pathString5);
+
+            // Check that the file has been downloaded 
+            if (file != null && file.ContentLength > 0)
+            {
+                // Get the file extension
+                string ext = file.ContentType.ToLower();
+
+                // Check file extension
+                if (ext != "image/jpg" &&
+                    ext != "image/jpeg" &&
+                    ext != "image/pjpeg" && // A few (рідкісне) image extension but sometimes used
+                    ext != "image/gif" &&
+                    ext != "image/x-png" &&
+                    ext != "image/png")
+                {
+                    using (Db db = new Db())
+                    {
+                        model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                        ModelState.AddModelError("", "The image was not uploaded - wrong image extension!");
+                        return View(model);
+                    }
+                }
+
+                // Assign variable with name of image
+                string imageName = file.FileName;
+
+                // Save the name of image in model DTO
+                using (Db db = new Db())
+                {
+                    ProductDTO dto = db.Products.Find(id);
+                    dto.ImageName = imageName;
+
+                    db.SaveChanges();
+                }
+
+                // Assign paths to the original and reduced image
+                var path = string.Format($"{pathString2}\\{imageName}");
+                var path2 = string.Format($"{pathString3}\\{imageName}");
+
+                // Save original image
+                file.SaveAs(path);
+
+                // Create and save reduced copy of image
+                WebImage img = new WebImage(file.InputStream);
+                img.Resize(200, 200);
+                img.Save(path2);
+            }
+            #endregion
+
+            // Redirect user 
+            return RedirectToAction("AddProduct");
+        }
+
+        // Add GET method the list of goods
+        // GET: Admin/Shop/Products
+        [HttpGet]
+        public ActionResult Products(int? page, int? catId)
+        {
+            // Assign model ProductVM with type List
+            List<ProductVM> listOfProductVM;
+
+            // Set the number of page
+            var pageNumber = page ?? 1; /* if the result returns null it will automatically be set to 1,
+                                               if it returns a value instead of 1 it will be this value */
+
+            using (Db db = new Db())
+            {
+                // Initialize List and fill in data
+                listOfProductVM = db.Products.ToArray()
+                    .Where(x => catId == null || catId == 0 || x.CategoryId == catId)
+                    .Select(x => new ProductVM(x))
+                    .ToList();
+
+                // Fill in the categories with data
+                ViewBag.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+
+                // Set the selected category
+                ViewBag.SelectedCat = catId.ToString();
+            }
+
+            // Set a page navigation
+            var onePageOfProducts = listOfProductVM.ToPagedList(pageNumber, 3); // 3 - the number of goods in page
+            ViewBag.onePageOfProducts = onePageOfProducts;
+
+            // Return View() with data
+            return View(listOfProductVM);
         }
     }
 }
---
+++ WebStore_Contrast/Models/ViewModels/Shop/ProductVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore_Contrast.Models.Data;
+
+namespace WebStore_Contrast.Models.ViewModels.Shop
+{
+    public class ProductVM
+    {
+        public ProductVM()
+        {
+
+        }
+
+        public ProductVM(ProductDTO row)
+        {
+            Id = row.Id;
+            Name = row.Name;
+            Slug = row.Slug;
+            Description = row.Description;
+            Price = row.Price;
+            CategoryName = row.CategoryName;
+            CategoryId = row.CategoryId;
+            ImageName = row.ImageName;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Slug { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CategoryName { get; set; }
+
+        [Required]
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+
+        public string ImageName { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<string> GalleryImages { get; set; }
+    }
+}
